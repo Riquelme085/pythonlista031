3806e7f15260fc638c80d5b332589cdeb9838a6b
NIG0181032 <quelmebiro@gmail.com>
2023-08-07 11:38:41 -0300

    questaos

---
+++ interlocao_strems/interpolacao_string.py
@@ -0,0 +1,7 @@
+nome = input("digite o seu primeiro nome: ")
+idade = int(input("digite sua idade: "))
+
+print("olá, ", nome, "!")
+print("tudo bem com você?")
+print("caramba", nome, "! você tem", idade, "anos? nem parece.")
+
---
+++ operadores/recursos.py
@@ -0,0 +1,5 @@
+import math
+
+raiz = math.sqrt(16) # raiz quadrada de 16
+print(f"raiz:{raiz}")
+print(f"tipo de var raiz")
